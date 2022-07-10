3157efe758d805d5638b0c7a139b15e25bee0ccf
ArulShan <arulshanmugam1@gmail.com>
2022-07-10 21:53:08 +0530

    modified by tester1

--- src/org/test/Test.java
+++ src/org/test/Test.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class Test {
 
 	public static void main(String[] args) {
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\arul\\eclipse-workspace\\Practice\\drivers\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", "D:\\clone\\ProjectOne\\drivers\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://www.facebook.com/");
 		driver.manage().window().maximize();
@@ -18,6 +18,7 @@ public class Test {
 		findElement2.sendKeys("1234");
 		WebElement findElement3 = driver.findElement(By.name("login"));
 		findElement3.click();
+		driver.quit();
 	}
-
+	
 }
